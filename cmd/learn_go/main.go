546a5b78b3eddb397ddae98518e70a29856dcb2e
seolman <tjfehdgns@gmail.com>
2026-01-15 09:09:22 +0900

    1-3: short variable declaration

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var smsSendingLimit int
-	var costPerSMS float64
-	var hasPermission bool
-	var username string
-	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+	messageStart := "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+	fmt.Println(messageStart, age, messageEnd)
 }
