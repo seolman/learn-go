06425902ad4d71b72f3e69e84b4ac0231804b821
seolman <tjfehdgns@gmail.com>
2026-01-15 09:42:42 +0900

    1-20: formatting strings in go

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	const secondsInMinute = 60
-	const minutesInHour = 60
-	const secondsInHour = secondsInMinute * minutesInHour
+	const name = "Saul Goodman"
+	const openRate = 30.5
+
+	msg := fmt.Sprintf("Hi %v, your open rate is %v percent\n", name, openRate)
 
 	// don't edit below this line
-	fmt.Println("number of seconds in an hour:", secondsInHour)
+
+	fmt.Print(msg)
 }
