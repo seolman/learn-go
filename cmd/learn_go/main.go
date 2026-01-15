3816c044fa76a256b2265d30f0948cc5d59b6111
seolman <tjfehdgns@gmail.com>
2026-01-15 09:07:47 +0900

    1-2: basic variables

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello there!")
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
