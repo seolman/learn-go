d6d6881d2af9189448671d37877630cb2208ec91
seolman <tjfehdgns@gmail.com>
2026-01-15 09:45:40 +0900

    1-22: fix bugs

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"unicode/utf8"
-)
+import "fmt"
 
 func main() {
-	const name = "🐻"
-	fmt.Printf("constant 'name' byte length: %d\n", len(name))
-	fmt.Printf("constant 'name' rune length: %d\n", utf8.RuneCountInString(name))
-	fmt.Println("=====================================")
-	fmt.Printf("Hi %s, so good to have you back in the arcanum\n", name)
+	var startup string = "Textio SMS service booting up..."
+	var message string = "Sending test message"
+	var confirmation string = "Message sent!"
+
+	// don't touch below this line
+
+	fmt.Println(startup)
+	fmt.Println(message)
+	fmt.Println(confirmation)
 }
