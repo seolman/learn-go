79f0acad255ed7b0dbe64c2ce04105fbe38bf2cf
seolman <tjfehdgns@gmail.com>
2026-01-16 08:44:05 +0900

    2-4: switch

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -2,16 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	messageLen := 10
-	maxMessageLen := 20
-	fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
-
-	// don't touch above this line
-
-	if messageLen < maxMessageLen {
-		fmt.Println("Message sent")
-	} else {
-		fmt.Println("Message not sent")
+func billingCost(plan string) float64 {
+	switch plan {
+	case "basic":
+		return 10.0
+	case "pro":
+		return 20.0
+	case "enterprise":
+		return 50.0
+	default:
+		return 0.0
 	}
+}
+
+// don't touch below this line
+
+func main() {
+	plan := "basic"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "pro"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "enterprise"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "free"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "unknown"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
 }
