73e2297b8f96c0de61f95f03091f51f2f8c918a6
seolman <tjfehdgns@gmail.com>
2026-01-15 09:44:36 +0900

    1-21: runes and string encoding

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -1,14 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	const name = "Saul Goodman"
-	const openRate = 30.5
-
-	msg := fmt.Sprintf("Hi %v, your open rate is %v percent\n", name, openRate)
-
-	// don't edit below this line
-
-	fmt.Print(msg)
+	const name = "🐻"
+	fmt.Printf("constant 'name' byte length: %d\n", len(name))
+	fmt.Printf("constant 'name' rune length: %d\n", utf8.RuneCountInString(name))
+	fmt.Println("=====================================")
+	fmt.Printf("Hi %s, so good to have you back in the arcanum\n", name)
 }
