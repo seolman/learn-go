90e62057e593f2ac6afd10a6f58049951c11fcde
seolman <tjfehdgns@gmail.com>
2026-01-16 23:10:12 +0900

    3-16: anonymous functions

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -1,9 +1,31 @@
 package main
 
-func reformat(message string, formatter func(string) string) string {
-	for range 3 {
-		message = formatter(message)
-	}
-	const prefix = "TEXTIO: "
-	return prefix + message
+import "fmt"
+
+func printReports(intro, body, outro string) {
+	printCostReport(func(s string) int {
+		return len(s) * 2
+	}, intro)
+	printCostReport(func(s string) int {
+		return len(s) * 3
+	}, body)
+	printCostReport(func(s string) int {
+		return len(s) * 4
+	}, outro)
+}
+
+// don't touch below this line
+
+func main() {
+	printReports(
+		"Welcome to the Hotel California",
+		"Such a lovely place",
+		"Plenty of room at the Hotel California",
+	)
+}
+
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
+	fmt.Println()
 }
