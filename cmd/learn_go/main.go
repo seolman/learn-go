ca4122bacc128b87d524d98802a7626bd705253b
seolman <tjfehdgns@gmail.com>
2026-01-16 09:13:33 +0900

    2-5: calculate balance

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -2,30 +2,30 @@ package main
 
 import "fmt"
 
-func billingCost(plan string) float64 {
-	switch plan {
-	case "basic":
-		return 10.0
-	case "pro":
-		return 20.0
-	case "enterprise":
-		return 50.0
-	default:
-		return 0.0
-	}
-}
-
-// don't touch below this line
-
 func main() {
-	plan := "basic"
-	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
-	plan = "pro"
-	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
-	plan = "enterprise"
-	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
-	plan = "free"
-	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
-	plan = "unknown"
-	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	var insufficientFundMessage string = "Purchase failed. Insufficient funds."
+	var purchaseSuccessMessage string = "Purchase successful."
+	var accountBalance float64 = 100.0
+	var bulkMessageCost float64 = 75.0
+	var isPremiumUser bool = true
+	var discountRate float64 = 0.10
+	var finalCost float64
+
+	// don't edit above this line
+
+	finalCost = bulkMessageCost
+	if isPremiumUser {
+		finalCost *= 1 - discountRate
+	}
+
+	if finalCost < accountBalance {
+		accountBalance -= finalCost
+		fmt.Println(purchaseSuccessMessage)
+	} else {
+		fmt.Println(insufficientFundMessage)
+	}
+
+	// don't edit below this line
+
+	fmt.Println("Account balance:", accountBalance)
 }
