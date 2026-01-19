43992160b1dfea136bbadf3f52c610baecfa5472
seolman <tjfehdgns@gmail.com>
2026-01-19 09:54:56 +0900

    8-1: arrays in go

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -1,9 +1,15 @@
 package main
 
-func countConnections(groupSize int) int {
-	connections := 0
-	for i := 1; i < groupSize; i++ {
-		connections += i
+func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]int) {
+	message := [3]string{
+		primary,
+		secondary,
+		tertiary,
 	}
-	return connections
+	lengths := [3]int{
+		len(primary),
+		len(secondary) + len(primary),
+		len(tertiary) + len(secondary) + len(primary),
+	}
+	return message, lengths
 }
