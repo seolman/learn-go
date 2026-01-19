e3229f9c7aeda664f6ea447dbc009fd41c0c384f
seolman <tjfehdgns@gmail.com>
2026-01-19 10:38:11 +0900

    8-12: range

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -1,17 +1,12 @@
 package main
 
-type cost struct {
-	day   int
-	value float64
-}
-
-func getDayCosts(costs []cost, day int) []float64 {
-	dayCosts := []float64{}
-	for i := range costs {
-		cost := costs[i]
-		if cost.day == day {
-			dayCosts = append(dayCosts, cost.value)
+func indexOfFirstBadWord(msgs []string, badWords []string) int {
+	for i, msg := range msgs {
+		for _, bad := range badWords {
+			if msg == bad {
+				return i
+			}
 		}
 	}
-	return dayCosts
+	return -1
 }
--- cmd/learn_go/main_test.go
+++ cmd/learn_go/main_test.go
@@ -2,71 +2,25 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 )
 
 func Test(t *testing.T) {
 	type testCase struct {
-		costs    []cost
-		day      int
-		expected []float64
+		msg      []string
+		badWords []string
+		expected int
 	}
 
 	runCases := []testCase{
-		{
-			costs: []cost{
-				{0, 1.0},
-				{1, 2.0},
-				{1, 3.1},
-				{5, 2.5},
-				{2, 3.6},
-				{1, 2.7},
-				{1, 3.3},
-			},
-			day: 1,
-			expected: []float64{
-				2.0,
-				3.1,
-				2.7,
-				3.3,
-			},
-		},
+		{[]string{"hey", "there", "john"}, []string{"crap", "shoot", "frick", "dang"}, -1},
+		{[]string{"ugh", "oh", "my", "frick"}, []string{"crap", "shoot", "frick", "dang"}, 3},
 	}
 
 	submitCases := append(runCases, []testCase{
-		{
-			costs: []cost{
-				{0, 1.0},
-				{1, 2.0},
-				{1, 3.1},
-				{2, 2.5},
-				{3, 3.1},
-				{3, 2.6},
-				{4, 3.34},
-			},
-			day: 4,
-			expected: []float64{
-				3.34,
-			},
-		},
-		{
-			costs: []cost{
-				{0, 1.0},
-				{10, 2.0},
-				{3, 3.1},
-				{2, 2.5},
-				{1, 3.6},
-				{2, 2.7},
-				{4, 56.34},
-				{13, 2.34},
-				{28, 1.34},
-				{25, 2.34},
-				{30, 4.34},
-			},
-			day:      5,
-			expected: []float64{},
-		},
+		{[]string{"what", "the", "shoot", "I", "hate", "that", "crap"}, []string{"crap", "shoot", "frick", "dang"}, 2},
+		{[]string{"crap", "shoot", "frick", "dang"}, []string{""}, -1},
+		{[]string{""}, nil, -1},
 	}...)
 
 	testCases := runCases
@@ -74,33 +28,37 @@ func Test(t *testing.T) {
 		testCases = submitCases
 	}
 
-	passCount := 0
-	failCount := 0
 	skipped := len(submitCases) - len(testCases)
 
+	passCount := 0
+	failCount := 0
+
 	for _, test := range testCases {
-		output := getDayCosts(test.costs, test.day)
-		if !reflect.DeepEqual(output, test.expected) {
+		output := indexOfFirstBadWord(test.msg, test.badWords)
+		if output != test.expected {
 			failCount++
 			t.Errorf(`---------------------------------
-Inputs:
+Test Failed:
+message:
 %v
-Expecting:
+bad words:
 %v
-Actual:
-%v
+Expecting:  %v
+Actual:     %v
 Fail
-`, sliceWithBullets(test.costs), sliceWithBullets(test.expected), sliceWithBullets(output))
+`, sliceWithBullets(test.msg), sliceWithBullets(test.badWords), test.expected, output)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
-Inputs:     %v
-Expecting:
+Test Passed:
+message:
 %v
-Actual:
+bad words:
 %v
+Expecting:  %v
+Actual:     %v
 Pass
-`, sliceWithBullets(test.costs), sliceWithBullets(test.expected), sliceWithBullets(output))
+`, sliceWithBullets(test.msg), sliceWithBullets(test.badWords), test.expected, output)
 		}
 	}
 
@@ -121,9 +79,9 @@ func sliceWithBullets[T any](slice []T) string {
 	}
 	output := ""
 	for i, item := range slice {
-		form := "  - %v\n"
+		form := "  - %#v\n"
 		if i == (len(slice) - 1) {
-			form = "  - %v"
+			form = "  - %#v"
 		}
 		output += fmt.Sprintf(form, item)
 	}
