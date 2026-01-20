f1557f7cd45c2bee2fbbe0ff5b40c5f2ac78be12
seolman <tjfehdgns@gmail.com>
2026-01-20 09:51:12 +0900

    10-11: update balance

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -1,13 +1,40 @@
 package main
 
-func (e *email) setMessage(newMessage string) {
-	e.message = newMessage
+import (
+	"errors"
+)
+
+type customer struct {
+	id      int
+	balance float64
 }
 
-// don't edit below this line
+type transactionType string
 
-type email struct {
-	message     string
-	fromAddress string
-	toAddress   string
+const (
+	transactionDeposit    transactionType = "deposit"
+	transactionWithdrawal transactionType = "withdrawal"
+)
+
+type transaction struct {
+	customerID      int
+	amount          float64
+	transactionType transactionType
+}
+
+// Don't touch above this line
+
+func updateBalance(c *customer, t transaction) error {
+	switch t.transactionType {
+	case transactionWithdrawal:
+		if c.balance < t.amount {
+			return errors.New("insufficient funds")
+		}
+		c.balance -= t.amount
+	case transactionDeposit:
+		c.balance += t.amount
+	default:
+		return errors.New("unknown transaction type")
+	}
+	return nil
 }
--- cmd/learn_go/main_test.go
+++ cmd/learn_go/main_test.go
@@ -5,43 +5,49 @@ import (
 	"testing"
 )
 
-func TestSetMessage(t *testing.T) {
+func TestUpdateBalance(t *testing.T) {
 	type testCase struct {
-		e          email
-		newMessage string
-		expected   string
+		name            string
+		initialCustomer customer
+		transaction     transaction
+		expectedBalance float64
+		expectError     bool
+		errorMessage    string
 	}
 
 	runCases := []testCase{
 		{
-			email{
-				message:     "My name is Lt. Aldo Raine and I'm putting together a special team, and I need me eight soldiers.",
-				fromAddress: "lt.aldo.raine@textio.com",
-				toAddress:   "army@textio.com",
-			},
-			"You just say bingo.",
-			"You just say bingo.",
+			name:            "Deposit operation",
+			initialCustomer: customer{id: 1, balance: 100.0},
+			transaction:     transaction{customerID: 1, amount: 50.0, transactionType: transactionDeposit},
+			expectedBalance: 150.0,
+			expectError:     false,
 		},
 		{
-			email{
-				message:     "Now, if one were to determine what attribute the German people share with a beast, it would be the cunning and the predatory instinct of a hawk.",
-				fromAddress: "col.hans.landa@textio.com",
-				toAddress:   "lapadite@textio.com",
-			},
-			"What a tremendously hostile world that a rat must endure.",
-			"What a tremendously hostile world that a rat must endure.",
+			name:            "Withdrawal operation",
+			initialCustomer: customer{id: 2, balance: 200.0},
+			transaction:     transaction{customerID: 2, amount: 100.0, transactionType: transactionWithdrawal},
+			expectedBalance: 100.0,
+			expectError:     false,
 		},
 	}
 
 	submitCases := append(runCases, []testCase{
 		{
-			email{
-				message:     "Nazi ain't got no humanity. They're the foot soldiers of a Jew-hatin', mass murderin' maniac and they need to be dee-stroyed.",
-				fromAddress: "lt.aldo.raine@textio.com",
-				toAddress:   "basterds@textio.com",
-			},
-			"I think this just might be my masterpiece.",
-			"I think this just might be my masterpiece.",
+			name:            "insufficient funds for withdrawal",
+			initialCustomer: customer{id: 3, balance: 50.0},
+			transaction:     transaction{customerID: 3, amount: 100.0, transactionType: transactionWithdrawal},
+			expectedBalance: 50.0,
+			expectError:     true,
+			errorMessage:    "insufficient funds",
+		},
+		{
+			name:            "unknown transaction type",
+			initialCustomer: customer{id: 4, balance: 100.0},
+			transaction:     transaction{customerID: 4, amount: 50.0, transactionType: "unknown"},
+			expectedBalance: 100.0,
+			expectError:     true,
+			errorMessage:    "unknown transaction type",
 		},
 	}...)
 
@@ -56,33 +62,40 @@ func TestSetMessage(t *testing.T) {
 	failCount := 0
 
 	for _, test := range testCases {
-		originalMessage := test.e.message
-		test.e.setMessage(test.newMessage)
-		if test.e.message != test.expected {
-			failCount++
-			t.Errorf(`---------------------------------
-Test Failed:
-  inputs:
-    * msg: %v
-    * newMessage: %v
-    * from: %v
-    * to: %v
-  expected: %v
-  actual: %v
-`, originalMessage, test.newMessage, test.e.fromAddress, test.e.toAddress, test.expected, test.e.message)
-		} else {
-			passCount++
-			fmt.Printf(`---------------------------------
-Test Passed:
-  inputs:
-    * msg: %v
-    * newMessage: %v
-    * from: %v
-    * to: %v
-  expected: %v
-  actual: %v
-`, originalMessage, test.newMessage, test.e.fromAddress, test.e.toAddress, test.expected, test.e.message)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			err := updateBalance(&test.initialCustomer, test.transaction)
+			failureMessage := ""
+
+			if (err != nil) != test.expectError {
+				failureMessage += "Unexpected error presence: expected an error but didn't get one, or vice versa.\n"
+			} else if err != nil && err.Error() != test.errorMessage {
+				failureMessage += "Incorrect error message.\n"
+			}
+
+			if test.initialCustomer.balance != test.expectedBalance {
+				failureMessage += "Balance not updated as expected.\n"
+			}
+
+			if failureMessage != "" {
+				failCount++
+				failureMessage = "FAIL\n" + failureMessage +
+					"Transaction: " + string(test.transaction.transactionType) +
+					fmt.Sprintf(", Amount: %.2f\n", test.transaction.amount) +
+					fmt.Sprintf("Expected balance: %.2f, Actual balance: %.2f", test.expectedBalance, test.initialCustomer.balance)
+				t.Errorf(`---------------------------------
+				%s
+`, failureMessage)
+			} else {
+				passCount++
+				successMessage := "PASSED\n" +
+					"Transaction: " + string(test.transaction.transactionType) +
+					fmt.Sprintf(", Amount: %.2f\n", test.transaction.amount) +
+					fmt.Sprintf("Expected balance: %.2f, Actual balance: %.2f", test.expectedBalance, test.initialCustomer.balance)
+				fmt.Printf(`---------------------------------
+%s
+`, successMessage)
+			}
+		})
 	}
 
 	fmt.Println("---------------------------------")
@@ -91,7 +104,6 @@ Test Passed:
 	} else {
 		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
 	}
-
 }
 
 // withSubmit is set at compile time depending
