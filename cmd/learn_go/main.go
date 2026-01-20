54ce450872a25bd3d2ba37a1c9bd5614647faeb1
seolman <tjfehdgns@gmail.com>
2026-01-20 09:58:10 +0900

    11-1: packages

--- cmd/learn_go/main.go
+++ cmd/learn_go/main.go
@@ -1,40 +1,14 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 )
 
-type customer struct {
-	id      int
-	balance float64
+func test(text string) {
+	fmt.Println(text)
 }
 
-type transactionType string
-
-const (
-	transactionDeposit    transactionType = "deposit"
-	transactionWithdrawal transactionType = "withdrawal"
-)
-
-type transaction struct {
-	customerID      int
-	amount          float64
-	transactionType transactionType
-}
-
-// Don't touch above this line
-
-func updateBalance(c *customer, t transaction) error {
-	switch t.transactionType {
-	case transactionWithdrawal:
-		if c.balance < t.amount {
-			return errors.New("insufficient funds")
-		}
-		c.balance -= t.amount
-	case transactionDeposit:
-		c.balance += t.amount
-	default:
-		return errors.New("unknown transaction type")
-	}
-	return nil
+func main() {
+	test("starting Textio server")
+	test("stopping Textio server")
 }
