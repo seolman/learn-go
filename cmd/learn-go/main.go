7379265857aa987d638872842d49831d40d99b86
seolman <tjfehdgns@gmail.com>
2026-01-21 11:33:09 +0900

    14-7: parametric contraints

--- cmd/learn-go/main.go
+++ cmd/learn-go/main.go
@@ -1,59 +1,76 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
 )
 
-func chargeForLineItem[T lineItem](newItem T, oldItems []T, balance float64) ([]T, float64, error) {
-	if newItem.GetCost() > balance {
-		return nil, 0.0, errors.New("insufficient funds")
-	}
-
-	oldItems = append(oldItems, newItem)
-	balance -= newItem.GetCost()
-
-	return oldItems, balance, nil
+type biller[C customer] interface {
+	Charge(C) bill
+	Name() string
 }
 
 // don't edit below this line
 
-type lineItem interface {
-	GetCost() float64
-	GetName() string
+type userBiller struct {
+	Plan string
 }
 
-type subscription struct {
-	userEmail string
-	startDate time.Time
-	interval  string
-}
-
-func (s subscription) GetName() string {
-	return fmt.Sprintf("%s subscription", s.interval)
-}
-
-func (s subscription) GetCost() float64 {
-	if s.interval == "monthly" {
-		return 25.00
+func (ub userBiller) Charge(u user) bill {
+	amount := 50.0
+	if ub.Plan == "pro" {
+		amount = 100.0
 	}
-	if s.interval == "yearly" {
-		return 250.00
+	return bill{
+		Customer: u,
+		Amount:   amount,
 	}
-	return 0.0
 }
 
-type oneTimeUsagePlan struct {
-	userEmail        string
-	numEmailsAllowed int
+func (sb userBiller) Name() string {
+	return fmt.Sprintf("%s user biller", sb.Plan)
 }
 
-func (otup oneTimeUsagePlan) GetName() string {
-	return fmt.Sprintf("one time usage plan with %v emails", otup.numEmailsAllowed)
+type orgBiller struct {
+	Plan string
 }
 
-func (otup oneTimeUsagePlan) GetCost() float64 {
-	const costPerEmail = 0.03
-	return float64(otup.numEmailsAllowed) * costPerEmail
+func (ob orgBiller) Name() string {
+	return fmt.Sprintf("%s org biller", ob.Plan)
+}
+
+func (ob orgBiller) Charge(o org) bill {
+	amount := 2000.0
+	if ob.Plan == "pro" {
+		amount = 3000.0
+	}
+	return bill{
+		Customer: o,
+		Amount:   amount,
+	}
+}
+
+type customer interface {
+	GetBillingEmail() string
+}
+
+type bill struct {
+	Customer customer
+	Amount   float64
+}
+
+type user struct {
+	UserEmail string
+}
+
+func (u user) GetBillingEmail() string {
+	return u.UserEmail
+}
+
+type org struct {
+	Admin user
+	Name  string
+}
+
+func (o org) GetBillingEmail() string {
+	return o.Admin.GetBillingEmail()
 }
--- cmd/learn-go/main_test.go
+++ cmd/learn-go/main_test.go
@@ -2,83 +2,47 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"testing"
-	"time"
 )
 
-func TestChargeForLineItem(t *testing.T) {
+func TestOrgBilling(t *testing.T) {
 	type testCase struct {
-		newItem           lineItem
-		oldItems          []lineItem
-		balance           float64
-		expected          []lineItem
-		expectedBalance   float64
-		expectedErrString string
+		biller         orgBiller
+		customer       org
+		expectedAmount float64
+		expectedEmail  string
 	}
 
 	runCases := []testCase{
 		{
-			newItem: subscription{
-				userEmail: "geralt@rivia.com",
-				startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-				interval:  "yearly",
+			biller: orgBiller{Plan: "pro"},
+			customer: org{
+				Admin: user{UserEmail: "jaskier@oxenfurt.com"},
+				Name:  "Oxenfurt",
 			},
-			oldItems: []lineItem{
-				subscription{
-					userEmail: "yen@vengerberg.com",
-					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-					interval:  "monthly",
-				},
-				oneTimeUsagePlan{
-					userEmail:        "triss@maribor",
-					numEmailsAllowed: 100,
-				},
+			expectedAmount: 3000,
+			expectedEmail:  "jaskier@oxenfurt.com",
+		},
+		{
+			biller: orgBiller{Plan: "basic"},
+			customer: org{
+				Admin: user{UserEmail: "vernon@temeria.com"},
+				Name:  "Temeria",
 			},
-			balance: 1000.00,
-			expected: []lineItem{
-				subscription{
-					userEmail: "yen@vengerberg.com",
-					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-					interval:  "monthly",
-				},
-				oneTimeUsagePlan{
-					userEmail:        "triss@maribor",
-					numEmailsAllowed: 100,
-				},
-				subscription{
-					userEmail: "geralt@rivia.com",
-					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-					interval:  "yearly",
-				},
-			},
-			expectedBalance:   750.00,
-			expectedErrString: "",
+			expectedAmount: 2000,
+			expectedEmail:  "vernon@temeria.com",
 		},
 	}
 
 	submitCases := append(runCases, []testCase{
 		{
-			newItem: subscription{
-				userEmail: "geralt@rivia.com",
-				startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-				interval:  "yearly",
+			biller: orgBiller{Plan: "pro"},
+			customer: org{
+				Admin: user{UserEmail: "fringilla@nilfgaard.com"},
+				Name:  "Nilfgaard",
 			},
-			oldItems: []lineItem{
-				subscription{
-					userEmail: "yen@vengerberg.com",
-					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-					interval:  "monthly",
-				},
-				oneTimeUsagePlan{
-					userEmail:        "triss@maribor",
-					numEmailsAllowed: 100,
-				},
-			},
-			balance:           200.00,
-			expected:          nil,
-			expectedBalance:   0.0,
-			expectedErrString: "insufficient funds",
+			expectedAmount: 3000,
+			expectedEmail:  "fringilla@nilfgaard.com",
 		},
 	}...)
 
@@ -91,98 +55,131 @@ func TestChargeForLineItem(t *testing.T) {
 	passCount := 0
 	failCount := 0
 
-	for _, test := range testCases {
-		oldItems := append([]lineItem(nil), test.oldItems...)
-		newItems, newBalance, err := chargeForLineItem(
-			test.newItem,
-			test.oldItems,
-			test.balance,
-		)
-		if (err != nil && err.Error() != test.expectedErrString) ||
-			(err == nil && test.expectedErrString != "") ||
-			!slices.Equal(newItems, test.expected) ||
-			newBalance != test.expectedBalance {
+	for i, test := range testCases {
+		err := testBiller(test.biller, test.customer, test.expectedAmount, test.expectedEmail)
+		if err != nil {
 			failCount++
 			t.Errorf(`---------------------------------
-Test Failed:
-  newItem:  %v
-  oldItems:
+OrgTest %d Failed:
 %v
-  balance:  %v
-  expected items:
-%v
-  expected balance: %v
-  expected error:   %v
-  actual items:
-%v
-  actual balance: %v
-  actual error:   %v
-`,
-				test.newItem,
-				sliceWithBullets(oldItems),
-				test.balance,
-				sliceWithBullets(test.expected),
-				test.expectedBalance,
-				test.expectedErrString,
-				sliceWithBullets(newItems),
-				newBalance,
-				err,
-			)
+`, i, err)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
-Test Passed:
-  newItem:  %v
-  oldItems:
-%v
-  balance:  %v
-  expected items:
-%v
-  expected balance: %v
-  expected error:   %v
-  actual items:
-%v
-  actual balance: %v
-  actual error:   %v
-`,
-				test.newItem,
-				sliceWithBullets(oldItems),
-				test.balance,
-				sliceWithBullets(test.expected),
-				test.expectedBalance,
-				test.expectedErrString,
-				sliceWithBullets(newItems),
-				newBalance,
-				err,
-			)
+OrgTest %d Passed:
+  biller:   %v
+  customer: %v
+`, i, test.biller, test.customer)
 		}
 	}
 
 	fmt.Println("---------------------------------")
 	if skipped > 0 {
-		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+		fmt.Printf("OrgBilling: %d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
 	} else {
-		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+		fmt.Printf("OrgBilling: %d passed, %d failed\n", passCount, failCount)
 	}
-
 }
 
-func sliceWithBullets[T any](slice []T) string {
-	if slice == nil {
-		return "  <nil>"
+func TestUserBilling(t *testing.T) {
+	type testCase struct {
+		biller         userBiller
+		customer       user
+		expectedAmount float64
+		expectedEmail  string
 	}
-	if len(slice) == 0 {
-		return "  []"
+
+	runCases := []testCase{
+		{
+			biller:         userBiller{Plan: "basic"},
+			customer:       user{UserEmail: "vesemir@kaermorhen.com"},
+			expectedAmount: 50,
+			expectedEmail:  "vesemir@kaermorhen.com",
+		},
+		{
+			biller:         userBiller{Plan: "pro"},
+			customer:       user{UserEmail: "zoltan@mahakam.com"},
+			expectedAmount: 100,
+			expectedEmail:  "zoltan@mahakam.com",
+		},
 	}
-	output := ""
-	for i, item := range slice {
-		form := "  - %v\n"
-		if i == (len(slice) - 1) {
-			form = "  - %v"
+
+	submitCases := append(runCases, []testCase{
+		{
+			biller:         userBiller{Plan: "pro"},
+			customer:       user{UserEmail: "extra@submit.com"},
+			expectedAmount: 100,
+			expectedEmail:  "extra@submit.com",
+		},
+	}...)
+
+	testCases := runCases
+	if withSubmit {
+		testCases = submitCases
+	}
+
+	skipped := len(submitCases) - len(testCases)
+	passCount := 0
+	failCount := 0
+
+	for i, test := range testCases {
+		err := testBiller(test.biller, test.customer, test.expectedAmount, test.expectedEmail)
+		if err != nil {
+			failCount++
+			t.Errorf(`---------------------------------
+UserTest %d Failed:
+%v
+`, i, err)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+UserTest %d Passed:
+  biller:   %v
+  customer: %v
+`, i, test.biller, test.customer)
 		}
-		output += fmt.Sprintf(form, item)
 	}
-	return output
+
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("UserBilling: %d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("UserBilling: %d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+func testBiller[C customer](
+	b biller[C],
+	c C,
+	expectedAmount float64,
+	expectedEmail string,
+) error {
+	currentBill := b.Charge(c)
+	name := b.Name()
+
+	if currentBill.Amount != expectedAmount ||
+		currentBill.Customer.GetBillingEmail() != expectedEmail {
+		return fmt.Errorf(`biller "%v" FAILED:
+  biller Type:     %T
+  customer Type:   %T
+  customer:        %v
+  expected amount: %v
+  expected email:  %v
+  actual amount:   %v
+  actual email:    %v
+`,
+			name,
+			b,
+			c,
+			c,
+			expectedAmount,
+			expectedEmail,
+			currentBill.Amount,
+			currentBill.Customer.GetBillingEmail(),
+		)
+	}
+
+	return nil
 }
 
 // withSubmit is set at compile time depending
