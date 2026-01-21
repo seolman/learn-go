c086d9a3dec05e5627a5ba737de6399af0ec9274
seolman <tjfehdgns@gmail.com>
2026-01-21 11:41:20 +0900

    15-1: lack of enums

--- cmd/learn-go/main_test.go
+++ cmd/learn-go/main_test.go
@@ -5,44 +5,53 @@ import (
 	"testing"
 )
 
-func TestOrgBilling(t *testing.T) {
+func TestHandleEmailBounce(t *testing.T) {
 	type testCase struct {
-		biller         orgBiller
-		customer       org
-		expectedAmount float64
-		expectedEmail  string
+		email           email
+		expectedError   string
+		expectedStatus  string
+		expectedBounces int
 	}
 
 	runCases := []testCase{
 		{
-			biller: orgBiller{Plan: "pro"},
-			customer: org{
-				Admin: user{UserEmail: "jaskier@oxenfurt.com"},
-				Name:  "Oxenfurt",
+			email: email{
+				status:    "email_bounced",
+				recipient: &user{email: "bugs@acme.inc"},
 			},
-			expectedAmount: 3000,
-			expectedEmail:  "jaskier@oxenfurt.com",
+			expectedError:   "<nil>",
+			expectedStatus:  "email_bounced",
+			expectedBounces: 1,
 		},
 		{
-			biller: orgBiller{Plan: "basic"},
-			customer: org{
-				Admin: user{UserEmail: "vernon@temeria.com"},
-				Name:  "Temeria",
+			email: email{
+				status:    "email_failed",
+				recipient: &user{email: "elmer@acme.inc"},
 			},
-			expectedAmount: 2000,
-			expectedEmail:  "vernon@temeria.com",
+			expectedError:   "error tracking user bounce: invalid event: email_failed",
+			expectedStatus:  "email_failed",
+			expectedBounces: 0,
+		},
+		{
+			email: email{
+				status:    "email_sent",
+				recipient: &user{email: "daffy@acme.inc"},
+			},
+			expectedError:   "error updating user status: invalid status: email_sent",
+			expectedStatus:  "",
+			expectedBounces: 0,
 		},
 	}
 
 	submitCases := append(runCases, []testCase{
 		{
-			biller: orgBiller{Plan: "pro"},
-			customer: org{
-				Admin: user{UserEmail: "fringilla@nilfgaard.com"},
-				Name:  "Nilfgaard",
+			email: email{
+				status:    "email_failed",
+				recipient: &user{email: "porky@acme.inc"},
 			},
-			expectedAmount: 3000,
-			expectedEmail:  "fringilla@nilfgaard.com",
+			expectedError:   "error tracking user bounce: invalid event: email_failed",
+			expectedStatus:  "email_failed",
+			expectedBounces: 0,
 		},
 	}...)
 
@@ -52,134 +61,41 @@ func TestOrgBilling(t *testing.T) {
 	}
 
 	skipped := len(submitCases) - len(testCases)
+
 	passCount := 0
 	failCount := 0
 
-	for i, test := range testCases {
-		err := testBiller(test.biller, test.customer, test.expectedAmount, test.expectedEmail)
-		if err != nil {
+	for _, test := range testCases {
+		a := &analytics{}
+		err := a.handleEmailBounce(test.email)
+		actualError := fmt.Sprintf("%v", err)
+		if actualError != test.expectedError {
 			failCount++
 			t.Errorf(`---------------------------------
-OrgTest %d Failed:
-%v
-`, i, err)
+Test Failed:
+  status:    %v
+  recipient: %v
+  expected error:   %v
+  actual error:     %v
+`, test.email.status, test.email.recipient.email, test.expectedError, actualError)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
-OrgTest %d Passed:
-  biller:   %v
-  customer: %v
-`, i, test.biller, test.customer)
+Test Passed:
+  status:    %v
+  recipient: %v
+  expected error:   %v
+  actual error:     %v
+`, test.email.status, test.email.recipient.email, test.expectedError, actualError)
 		}
 	}
 
 	fmt.Println("---------------------------------")
 	if skipped > 0 {
-		fmt.Printf("OrgBilling: %d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
 	} else {
-		fmt.Printf("OrgBilling: %d passed, %d failed\n", passCount, failCount)
+		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
 	}
-}
-
-func TestUserBilling(t *testing.T) {
-	type testCase struct {
-		biller         userBiller
-		customer       user
-		expectedAmount float64
-		expectedEmail  string
-	}
-
-	runCases := []testCase{
-		{
-			biller:         userBiller{Plan: "basic"},
-			customer:       user{UserEmail: "vesemir@kaermorhen.com"},
-			expectedAmount: 50,
-			expectedEmail:  "vesemir@kaermorhen.com",
-		},
-		{
-			biller:         userBiller{Plan: "pro"},
-			customer:       user{UserEmail: "zoltan@mahakam.com"},
-			expectedAmount: 100,
-			expectedEmail:  "zoltan@mahakam.com",
-		},
-	}
-
-	submitCases := append(runCases, []testCase{
-		{
-			biller:         userBiller{Plan: "pro"},
-			customer:       user{UserEmail: "extra@submit.com"},
-			expectedAmount: 100,
-			expectedEmail:  "extra@submit.com",
-		},
-	}...)
-
-	testCases := runCases
-	if withSubmit {
-		testCases = submitCases
-	}
-
-	skipped := len(submitCases) - len(testCases)
-	passCount := 0
-	failCount := 0
-
-	for i, test := range testCases {
-		err := testBiller(test.biller, test.customer, test.expectedAmount, test.expectedEmail)
-		if err != nil {
-			failCount++
-			t.Errorf(`---------------------------------
-UserTest %d Failed:
-%v
-`, i, err)
-		} else {
-			passCount++
-			fmt.Printf(`---------------------------------
-UserTest %d Passed:
-  biller:   %v
-  customer: %v
-`, i, test.biller, test.customer)
-		}
-	}
-
-	fmt.Println("---------------------------------")
-	if skipped > 0 {
-		fmt.Printf("UserBilling: %d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
-	} else {
-		fmt.Printf("UserBilling: %d passed, %d failed\n", passCount, failCount)
-	}
-}
-
-func testBiller[C customer](
-	b biller[C],
-	c C,
-	expectedAmount float64,
-	expectedEmail string,
-) error {
-	currentBill := b.Charge(c)
-	name := b.Name()
-
-	if currentBill.Amount != expectedAmount ||
-		currentBill.Customer.GetBillingEmail() != expectedEmail {
-		return fmt.Errorf(`biller "%v" FAILED:
-  biller Type:     %T
-  customer Type:   %T
-  customer:        %v
-  expected amount: %v
-  expected email:  %v
-  actual amount:   %v
-  actual email:    %v
-`,
-			name,
-			b,
-			c,
-			c,
-			expectedAmount,
-			expectedEmail,
-			currentBill.Amount,
-			currentBill.Customer.GetBillingEmail(),
-		)
-	}
-
-	return nil
 }
 
 // withSubmit is set at compile time depending
